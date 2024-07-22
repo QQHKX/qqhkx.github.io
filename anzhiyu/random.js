16957315884fdc4415ded3983b8d90e020bc8d8a
QQHKX <1816078482@qq.com>
2024-07-22 19:51:02 +0800

    Site updated: 2024-07-22 19:51:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/19/博客发文教程/","2024/06/29/hello-world/"];function toRandomPost(){
+var posts=["2024/06/29/hello-world/","2024/07/19/博客发文教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
