2f11634143101fd333ad764b2c3e285f392b48be
QQHKX <1816078482@qq.com>
2024-07-23 16:58:00 +0800

    Site updated: 2024-07-23 16:58:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/29/hello-world/","2024/07/19/博客发文教程/"];function toRandomPost(){
+var posts=["2024/07/24/嘉祥成华WiFi密码收录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
