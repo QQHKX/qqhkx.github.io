c3ab482d10fecb652a80a5b1b6fdbbf04e2bfff5
QQHKX <1816078482@qq.com>
2024-08-07 20:37:04 +0800

    Site updated: 2024-08-07 20:37:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/25/C-Socket实现传文件和加密通讯/","2024/08/08/晚自习时钟/","2024/07/24/嘉祥成华WiFi密码收录/"];function toRandomPost(){
+var posts=["2024/07/25/C-Socket实现传文件和加密通讯/","2024/07/24/嘉祥成华WiFi密码收录/","2024/08/08/晚自习时钟/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
