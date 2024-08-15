14a1ac3cd17af2e951cb72dc07d6a4805e205050
QQHKX <1816078482@qq.com>
2024-08-15 18:21:07 +0800

    Site updated: 2024-08-15 18:21:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/25/C-Socket实现传文件和加密通讯/","2024/08/08/晚自习时钟/","2024/08/14/QQHKX小店开业啦/","2024/07/24/嘉祥成华WiFi密码收录/"];function toRandomPost(){
+var posts=["2024/08/14/QQHKX小店开业啦/","2024/07/25/C-Socket实现传文件和加密通讯/","2024/08/16/什么是Redis？/","2024/08/08/晚自习时钟/","2024/08/16/Redis缓存管理机制/","2024/07/24/嘉祥成华WiFi密码收录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
