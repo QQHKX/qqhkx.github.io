51e09e3fc55eb7d427b652c2b9caeb46c4e77b81
QQHKX <1816078482@qq.com>
2024-10-04 13:25:38 +0800

    Site updated: 2024-10-04 13:25:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/25/C-Socket实现传文件和加密通讯/","2024/08/16/Redis缓存管理机制/","2024/09/23/VMware-Workstation-Pro-17下载及激活教程/","2024/08/16/什么是Redis？/","2024/08/08/晚自习时钟/"];function toRandomPost(){
+var posts=["2024/07/25/C-Socket实现传文件和加密通讯/","2024/08/16/Redis缓存管理机制/","2024/09/23/VMware-Workstation-Pro-17下载及激活教程/","2024/08/16/什么是Redis？/","2024/08/08/晚自习时钟/","2024/10/05/欢迎使用QQHKX-API/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
